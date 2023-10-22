7dfa57c2505a660307c4b36e716f4896c2a1d73c
Felitendo <Felendo@web.de>
2023-10-22 20:17:09 +0200

    Update ignore.js

--- ignore.js
+++ ignore.js
@@ -1,3 +1,16 @@
-const Test = "Test!";
+const { exec } = require('child_process');
 
-console.log(Test);
+// Führt den Befehl "npm start" im aktuellen Verzeichnis aus
+const child = exec('npm start');
+
+child.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+child.on('close', (code) => {
+  console.log(`Child process exited with code ${code}`);
+});
