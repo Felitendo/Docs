46581b3d2c54a9261cf2ebaf7ead0345cee0971f
Felitendo <Felendo@web.de>
2023-10-22 20:49:45 +0200

    Indexed + published

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,12 +63,17 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },   
       navbar: {
         title: 'Felo Docs',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
-        },
+        }, 
         items: [
           {
             type: 'docSidebar',
